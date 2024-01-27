2c6a777499be43f9e92365e23947f016be29d1a7
AndreSteyn0 <apa.steyn2@gmail.com>
2024-01-27 12:12:09 +0200

    Added validation to the contact form.

--- Projects/Personal_Website/andre-steyn/src/app/components/contact/contact.component.ts
+++ Projects/Personal_Website/andre-steyn/src/app/components/contact/contact.component.ts
@@ -1,20 +1,45 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent implements OnInit{
-  constructor(@Inject(PLATFORM_ID) private platformId: Object){}
+export class ContactComponent implements OnInit {
+  public contactForm!: FormGroup;
+  public showSuccessMessage = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       AOS.init();
     }
+
+    this.contactForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      message: new FormControl(null, Validators.required),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.contactForm);
+    this.contactForm.reset();
+    this.showSuccessMessage = true;
+    setTimeout(() => {
+      this.showSuccessMessage = false;
+    }, 5000);
   }
 }
